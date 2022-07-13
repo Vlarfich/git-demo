0e2bf0ff621c5684825b1a47cc68f37e5de93e6e
Vlado <vladzhu607@gmail.com>
2022-07-13 21:13:05 +0300

    first commit

---
+++ src/epam/EPAM.java
@@ -0,0 +1,9 @@
+package epam;
+import java.util.*;
+public class EPAM {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String in = sc.nextLine();
+		System.out.println("Hello, " + in);
+	}
+}
